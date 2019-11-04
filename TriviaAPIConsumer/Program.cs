7a444b6841283f119633531abde141ef7c7d6641
CalvinSithideth <calvin.sithideth@gmail.com>
2019-11-04 14:46:30 -0800

    Add project files.

---
+++ TriviaAPIConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Retrieving trivia questions....");
+
+            TriviaClient triviaClient = new TriviaClient();
+            string result = await triviaClient.GetTriviaQuestions();
+
+            Console.WriteLine("Recieved trivia questions. \n");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
